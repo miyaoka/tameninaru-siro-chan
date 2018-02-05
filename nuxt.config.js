a2762320879c3e3fe8071d19abb3352f88e99ae2
Masaya Kazama <miyan@t-p.jp>
2018-02-06 00:12:24 +0900

    Change site title

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,8 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    titleTemplate: (titleChunk) => (titleChunk ? `${titleChunk} - ` : '') + 'tameninaru-siro-chan',
+    titleTemplate: (titleChunk) =>
+      (titleChunk ? `${titleChunk} - ` : '') + 'ためになるシロちゃんのミックス動画プレイヤー',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
