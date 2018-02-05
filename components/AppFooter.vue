d7c27c6f0f63211ac10faadbe9cbbc9dd6ffa37a
Masaya Kazama <miyan@t-p.jp>
2018-02-06 00:15:37 +0900

    Add header and footer

---
+++ components/AppFooter.vue
@@ -0,0 +1,22 @@
+<template>
+<footer class="AppFooter">
+  <div class="links">
+    <a href="https://github.com/miyaoka/tameninaru-siro-chan" target="_blank" rel="noopener"><icon name="github" scale="2"/></a>
+    <a href="https://twitter.com/miyaoka" target="_blank" rel="noopener"><icon name="twitter" scale="2"/></a>
+  </div>
+</footer>
+</template>
+
+<style lang="scss" scoped>
+.AppFooter {
+  padding: 2rem;
+}
+.links {
+  text-align: center;
+
+  a {
+    color: #333;
+    margin: 1rem;
+  }
+}
+</style>
---
+++ components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+<header class="AppHeader">
+  <div>ためになるシロちゃんのミックス動画プレイヤー</div>
+  <nav>
+    <nuxt-link to="/" exact>プレイヤー</nuxt-link> /
+    <nuxt-link to="/calendar">カレンダー</nuxt-link> /
+    <nuxt-link to="/videos">いちらん</nuxt-link>
+  </nav>
+</header>
+</template>
+
+<style lang="scss" scoped>
+.nuxt-link-active {
+  border-bottom: 2px solid #80d4ef;
+}
+.AppHeader {
+  margin-bottom: 2rem;
+  text-align: center;
+}
+</style>
