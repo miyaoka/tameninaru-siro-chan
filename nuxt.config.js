8f9778c86c2ea738290b062ec5ff7a5faaf967e3
Masaya Kazama <miyan@t-p.jp>
2018-02-06 00:57:20 +0900

    Add ogp-image

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,12 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' },
+      {
+        property: 'og:image',
+        content: 'https://tameninaru-siro-chan.netlify.com/img/ogp-dolphin.jpg',
+        hid: 'ogImage'
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' },
